e0201001076f74ce206f53aac92d8f27e3c10c88
astamato <alejandra.stamato@gmail.com>
2023-11-25 16:20:59 +0100

    Updates compose to beta01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,16 +48,16 @@ android {
 }
 
 dependencies {
-    val composeVersion = "1.6.0-alpha08"
+    val composeVersion = "1.6.0-beta01"
     val lifecycleVersion = "2.6.2"
-    val navVersion = "2.7.2"
+    val navVersion = "2.7.5"
 
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.foundation:foundation:$composeVersion")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.1")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
